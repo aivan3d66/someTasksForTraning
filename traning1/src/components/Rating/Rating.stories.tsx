36193d7a4af3ea3b69a8e5a45d31636d3d2e0443
Ivan <adamovskij00@mail.ru>
2021-12-28 20:57:27 +0300

    add controlled Rating

---
+++ traning1/src/components/Rating/Rating.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Rating from './Rating';
+
+export default {
+  title: 'Rating stories',
+  component: Rating,
+}
+
+export const EmptyStars = () => <Rating value={0}/>;
+export const Rating1 = () => <Rating value={1} />;
+export const Rating2 = () => <Rating value={2}/>;
+export const Rating3 = () => <Rating value={3}/>;
+export const Rating4 = () => <Rating value={4}/>;
+export const Rating5 = () => <Rating value={5}/>;
+
+
