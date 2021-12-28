799922c82953a0c57d1d645d6e45285879ac98cf
Ivan <adamovskij00@mail.ru>
2021-12-28 20:59:49 +0300

    add UncontrolledRating.stories

---
+++ traning1/src/components/UncontrolledRating/UncontrolledRating.stories.tsx
@@ -0,0 +1,9 @@
+import UncontrolledRating from "./UncontrolledRating";
+import React from "react";
+
+export default {
+  title: 'UncontrolledRating stories',
+  component: UncontrolledRating,
+}
+
+export const StarRating = () => <UncontrolledRating/>;
