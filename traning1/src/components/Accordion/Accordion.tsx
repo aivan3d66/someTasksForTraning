b60079e7e5f2db685136a226d624fda2ea2f8b0e
Ivan <adamovskij00@mail.ru>
2021-12-28 20:52:23 +0300

    add new tags of Accordion

--- traning1/src/components/Accordion/Accordion.tsx
+++ traning1/src/components/Accordion/Accordion.tsx
@@ -12,40 +12,43 @@ function Accordion(props: PropsType) {
 
 export function AccordionTitle(props: PropsType) {
   console.log("AccordionTitle rendering");
-  return <h3 className="accordion__title"
-             onClick={props.onChange}
-
-  >{props.title}</h3>
+  return (
+    <h3 className="accordion__title"
+        onClick={props.onChange}
+    >
+      {props.title}
+    </h3>
+  )
 }
 
 
 export function AccordionBody() {
   console.log("AccordionBody rendering");
   return (
-    <ul className="accordion__items">
-      <li className="accordion__item">
+    <div className="accordion__items">
+      <p className="accordion__item">
         The other two, slight air and purging fire,<br/>
         Are both with thee, wherever I abide;<br/>
         The first my thought, the other my desire,<br/>
         These present-absent with swift motion slide.<br/>
-      </li>
-      <li className="accordion__item">
+      </p>
+      <p className="accordion__item">
         For when these quicker elements are gone<br/>
         In tender embassy of love to thee,<br/>
         My life, being made of four, with two alone<br/>
         Sinks down to death, oppress'd with melancholy;<br/>
-      </li>
-      <li className="accordion__item">
+      </p>
+      <p className="accordion__item">
         Until life's composition be recured<br/>
         By those swift messengers return'd from thee,<br/>
         Who even but now come back again, assured<br/>
         Of thy fair health, recounting it to me:<br/>
-      </li>
-      <li className="accordion__item">
+      </p>
+      <p className="accordion__item">
         This told, I joy; but then no longer glad,<br/>
         I send them back again and straight grow sad.<br/>
-      </li>
-    </ul>
+      </p>
+    </div>
   )
 }
 
