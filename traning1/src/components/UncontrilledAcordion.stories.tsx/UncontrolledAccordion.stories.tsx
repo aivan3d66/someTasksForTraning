46eb691e51cf7e19bf9b777c69588047e46b1989
Ivan <adamovskij00@mail.ru>
2021-12-28 20:59:00 +0300

    add UncontrolledAccordion.stories

---
+++ traning1/src/components/UncontrilledAcordion.stories.tsx/UncontrolledAccordion.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import UncontrolledAccordion from "./UncontrolledAcordion";
+
+export default {
+  title: 'UncontrolledAccordion stories',
+  component: UncontrolledAccordion,
+}
+
+export const MenuChanging = () => <UncontrolledAccordion/>
