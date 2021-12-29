dedf7d3372617b6601292ce772198bfeb5963b20
Ivan <adamovskij00@mail.ru>
2021-12-29 20:00:23 +0300

    add App logic

--- traning2/src/App.tsx
+++ traning2/src/App.tsx
@@ -1,8 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {ControlButtons} from "./components/controlComponent/ControlButtons";
+import {ScreenComponent} from "./components/screenComponent/ScreenComponent";
+
+const ERROR_MESSAGE = "Достигнуто максимальное чисто счётчика"
 
 function App() {
+  const [screenValue, setScreenValue] = useState<number>(0);
+  const [error, setError] = useState<string>("");
+
+  const onIncrementHandler = () => {
+    if (screenValue < 5) {
+      setError("")
+      setScreenValue(screenValue + 1);
+    } else {
+      setError(ERROR_MESSAGE);
+    }
+  }
+
+  const onResetHandler = () => {
+    setScreenValue(0);
+    setError("")
+  }
+
   return (
     <div className="App">
       <header className="App-header">
