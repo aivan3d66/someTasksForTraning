6973250c9dd4a016d44be4995227bbf9a0eb3ff0
Ivan <adamovskij00@mail.ru>
2022-01-06 18:44:14 +0300

    add messages constants

--- traning2/src/App.tsx
+++ traning2/src/App.tsx
@@ -9,7 +9,10 @@ import {restoreCounter, saveCounter} from "./components/localStorage";
 export type OnIncrementHandler = () => void;
 export type OnResetHandler = () => void;
 
-const ERROR_MESSAGE: string = "Достигнуто максимальное число счётчика";
+const ERROR_MESSAGE: string = "Max counter value";
+const INCORRECT_VALUE_MESSAGE: string = "Incorrect value, must be > 0";
+const INCORRECT_MAX_VALUE_MESSAGE: string = "Incorrect value, must be > start value";
+const ENTER_VALUE_MESSAGE: string = "Enter values and press 'Set'";
 const COUNT_TICK: number = 1;
 
 function App() {
