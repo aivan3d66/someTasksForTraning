8a46f077219c8432d764ba464d04dd30ae6156b7
Ivan <adamovskij00@mail.ru>
2022-02-02 16:23:42 +0300

    add actions, AC and INC/GET VALUES FROM LS logic to counterReducer

--- traning2/src/bll/counterReducer.tsx
+++ traning2/src/bll/counterReducer.tsx
@@ -1,9 +1,35 @@
 type InitialStateType = typeof initialState
 
+const INC_COUNTER = 'INC_COUNTER'
+const GET_VALUES_FROM_LOCAL_STORAGE = 'GET_VALUES_FROM_LOCAL_STORAGE'
+
+type IncCounterValueType = ReturnType<typeof incCounterValueAC>
+type GetValuesFromLocalStorageType = ReturnType<typeof getValuesFromLocalStorageAC>
+
+type ActionType = IncCounterValueType | GetValuesFromLocalStorageType
+
 const initialState = {
-  value: 0
+  startValue: 0,
 }
 
-export const counterReducer = (state: InitialStateType = initialState, action: any): InitialStateType => {
-  return {...state}
+export const counterReducer = (state: InitialStateType = initialState, action: ActionType): InitialStateType => {
+  switch (action.type) {
+    case INC_COUNTER: {
+      return {
+        ...state, startValue: state.startValue + 1
+      }
+    }
+
+    case GET_VALUES_FROM_LOCAL_STORAGE: {
+      return {
+        ...state, startValue: action.value
+      }
+    }
+
+    default:
+      return state;
+  }
 }
+
+export const incCounterValueAC = () => ({type: INC_COUNTER} as const)
+export const getValuesFromLocalStorageAC = (value: number) => ({type: GET_VALUES_FROM_LOCAL_STORAGE, value} as const)
