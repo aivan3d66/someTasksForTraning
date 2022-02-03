5228daf85d17bb310180e13231fee344562c6411
Ivan <adamovskij00@mail.ru>
2022-02-03 18:27:48 +0300

    add upd SuperInput logic

--- traning2/src/components/SuperInput/SuperInputText.tsx
+++ traning2/src/components/SuperInput/SuperInputText.tsx
@@ -1,14 +1,14 @@
-import React, {ChangeEvent, DetailedHTMLProps, InputHTMLAttributes, KeyboardEvent, useState} from 'react'
+import React, {ChangeEvent, DetailedHTMLProps, InputHTMLAttributes, KeyboardEvent} from 'react'
 import s from './SuperInputText.module.css'
-import {useDispatch, useSelector} from "react-redux";
+import {useSelector} from "react-redux";
 import {AppStateType} from "../../bll/store";
 import { MESSAGES } from '../../App';
-import {setError, setMessage} from "../../bll/counterReducer";
 
 type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 type SuperInputTextPropsType = DefaultInputPropsType & {
-  onEnter?: () => void
+  onEnter?: () => void,
+  red: boolean
 }
 
 const SuperInput: React.FC<SuperInputTextPropsType> = (
@@ -18,39 +18,16 @@ const SuperInput: React.FC<SuperInputTextPropsType> = (
     onKeyPress,
     onEnter,
     className,
+    red,
     ...restProps
   }
 ) => {
-
-  const [red, setRed] = useState<boolean>(false);
-
-  const dispatch = useDispatch();
-  const value = useSelector<AppStateType, number>(state => state.counter.value);
-  const maxValue = useSelector<AppStateType, number>(state => state.counter.maxValue);
   const message = useSelector<AppStateType, string>(state => state.counter.message);
 
-
   const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-    if (+e.currentTarget.value < 0) {
-      setRed(true)
-      dispatch(setError(true))
-      dispatch(setMessage(MESSAGES.INCORRECT_VALUE_MESSAGE))
-    } else if (value > maxValue) {
-      setRed(true)
-      dispatch(setError(true))
-      dispatch(setMessage(MESSAGES.INCORRECT_START_VALUE_MESSAGE))
-    } else if (value < maxValue) {
-      setRed(false)
-      dispatch(setError(false))
-      dispatch(setMessage(''))
-    } else {
-      setRed(false)
-      dispatch(setError(false))
-      dispatch(setMessage(''))
-    }
-
     onChange && onChange(e)
   }
+
   const onKeyPressCallback = (e: KeyboardEvent<HTMLInputElement>) => {
     onKeyPress && onKeyPress(e);
 
