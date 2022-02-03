0329ffc2db33ef1150d171140575d09aa5c8f2af
Ivan <adamovskij00@mail.ru>
2022-02-03 13:50:15 +0300

    add counterReducer AC with state change logic

--- traning2/src/bll/counterReducer.tsx
+++ traning2/src/bll/counterReducer.tsx
@@ -36,13 +36,37 @@ export const counterReducer = (state: InitialStateType = initialState, action: A
   switch (action.type) {
     case INC_COUNTER: {
       return {
-        ...state, startValue: state.startValue + 1
+        ...state, value: state.value + 1
+      }
+    }
+
+    case SET_START_VALUE: {
+      return {
+        ...state, value: action.startValue
+      }
+    }
+
+    case SET_MAX_VALUE: {
+      return {
+        ...state, maxValue: action.maxValue
       }
     }
 
     case GET_VALUES_FROM_LOCAL_STORAGE: {
       return {
-        ...state, startValue: action.value
+        ...state, value: action.value
+      }
+    }
+
+    case SET_ERROR: {
+      return {
+        ...state, error: action.value
+      }
+    }
+
+    case SET_MESSAGE: {
+      return {
+        ...state, message: action.message
       }
     }
 
@@ -51,5 +75,16 @@ export const counterReducer = (state: InitialStateType = initialState, action: A
   }
 }
 
-export const incCounterValueAC = () => ({type: INC_COUNTER} as const)
-export const getValuesFromLocalStorageAC = (value: number) => ({type: GET_VALUES_FROM_LOCAL_STORAGE, value} as const)
+export const incCounterValueAC = () => ({type: INC_COUNTER} as const);
+export const setError = (value: boolean) => ({type: SET_ERROR, value} as const);
+export const setMessage = (message: string) => ({type: SET_MESSAGE, message} as const);
+export const setMaxCounterValueAC = (maxValue: number) => ({type: SET_MAX_VALUE, maxValue} as const);
+export const setStartCounterValueAC = (startValue: number) => ({type: SET_START_VALUE, startValue} as const);
+export const getValuesFromLocalStorageAC = (value: number) => ({type: GET_VALUES_FROM_LOCAL_STORAGE, value} as const);
+
+
+export const getValueFromLocalStorageTC = () => (dispatch: Dispatch) => {
+  let value = loadState().counter.value
+  dispatch(getValuesFromLocalStorageAC(value));
+}
+
