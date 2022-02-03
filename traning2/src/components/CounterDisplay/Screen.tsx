c27609c0f34ddd49735ed3d6e27732e2ce4de136
Ivan <adamovskij00@mail.ru>
2022-02-03 13:48:02 +0300

    add refactoring Screen with redux

--- traning2/src/components/CounterDisplay/Screen.tsx
+++ traning2/src/components/CounterDisplay/Screen.tsx
@@ -1,19 +1,15 @@
 import React from "react";
+import {useSelector} from "react-redux";
+import {AppStateType} from "../../bll/store";
 
 type ScreenComponentProps = {
   startValue: number,
   error: boolean,
-  message: string,
 }
 
-export const Screen: React.FC<ScreenComponentProps> = (
-  {
-    startValue,
-    error,
-    message
-  }
-) => {
+export const Screen: React.FC<ScreenComponentProps> = ({startValue, error}) => {
   const finalClassName = `${error ? "screenContentRed" : "screenContent"}`
+  const message = useSelector<AppStateType, string>(state => state.counter.message)
 
   return (
     <div className={finalClassName}>
