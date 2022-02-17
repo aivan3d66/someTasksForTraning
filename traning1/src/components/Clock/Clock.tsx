5889db0db27020052c5f4ddd7aafc53c90344c4a
Ivan <adamovskij00@mail.ru>
2022-02-17 15:27:41 +0300

    add time fix '0'

--- traning1/src/components/Clock/Clock.tsx
+++ traning1/src/components/Clock/Clock.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 type PropsType = {
 
@@ -6,13 +6,31 @@ type PropsType = {
 
 export const Clock: React.FC<PropsType> = (props) => {
   const [date, setDate] = useState(new Date());
+  useEffect(() => {
+    setInterval(() => {
+      setDate(new Date())
+    }, 1000)
+  })
+
+  const secondsString = date.getSeconds() < 10
+    ? '0' + date.getSeconds()
+    : date.getSeconds()
+
+  const minutesString = date.getMinutes() < 10
+    ? '0' + date.getMinutes()
+    : date.getMinutes()
+
+  const hoursString = date.getHours() < 10
+    ? '0' + date.getHours()
+    : date.getHours()
+
   return (
     <div>
-      <span>{date.getHours()}</span>
+      <span>{hoursString}</span>
       :
-      <span>{date.getMinutes()}</span>
+      <span>{minutesString}</span>
       :
-      <span>{date.getSeconds()}</span>
+      <span>{secondsString}</span>
     </div>
   )
 }
