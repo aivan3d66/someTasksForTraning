9eb75270eb0fc24b01ca3745768ecd0cbc459784
Ivan <adamovskij00@mail.ru>
2022-02-17 15:45:52 +0300

    add setInterval and clearInterval in useEffect Clock

--- traning1/src/components/Clock/Clock.tsx
+++ traning1/src/components/Clock/Clock.tsx
@@ -4,13 +4,18 @@ type PropsType = {
 
 }
 
-export const Clock: React.FC<PropsType> = (props) => {
+export const Clock: React.FC<PropsType> = () => {
   const [date, setDate] = useState(new Date());
+
   useEffect(() => {
-    setInterval(() => {
-      setDate(new Date())
-    }, 1000)
-  })
+    const intervalID = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalID);
+    }
+  }, [])
 
   const getCorrectTimeString = (number: number) =>  number < 10 ? '0' + number : number
 
