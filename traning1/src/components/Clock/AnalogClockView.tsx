67f97fe229d5540aaec09af120895599f00d6c48
Ivan <adamovskij00@mail.ru>
2022-02-23 14:27:26 +0300

    add clock styles atr

--- traning1/src/components/Clock/AnalogClockView.tsx
+++ traning1/src/components/Clock/AnalogClockView.tsx
@@ -16,9 +16,9 @@ export const AnalogClockView: React.FC<ClockViewPropsType> = ({date}) => {
   return (
     <div className={"clock"}>
       <div className={"analog-clock"}>
-        <div className={"dial seconds"} />
-        <div className={"dial minutes"} />
-        <div className={"dial hours"} />
+        <div className={"dial seconds"} style={secondsStyle} />
+        <div className={"dial minutes"} style={minutesStyle} />
+        <div className={"dial hours"} style={hoursStyle} />
       </div>
     </div>
   )
