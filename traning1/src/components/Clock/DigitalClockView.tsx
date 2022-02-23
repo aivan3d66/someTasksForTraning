11ffbe66c4b701bde1fc98660f7ed47bb2c79d05
Ivan <adamovskij00@mail.ru>
2022-02-23 19:54:12 +0300

    add fix of clock time

--- traning1/src/components/Clock/DigitalClockView.tsx
+++ traning1/src/components/Clock/DigitalClockView.tsx
@@ -4,11 +4,11 @@ import {ClockViewPropsType, getCorrectTimeString} from "./Clock";
 export const DigitalClockView: React.FC<ClockViewPropsType> = ({date}) => {
   return (
     <>
-      <span>{getCorrectTimeString(date.getSeconds())}</span>
+      <span>{getCorrectTimeString(date.getHours())}</span>
       :
       <span>{getCorrectTimeString(date.getMinutes())}</span>
       :
-      <span>{getCorrectTimeString(date.getHours())}</span>
+      <span>{getCorrectTimeString(date.getSeconds())}</span>
     </>
   )
 }
