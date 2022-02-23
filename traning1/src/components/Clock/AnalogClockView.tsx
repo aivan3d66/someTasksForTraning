90931b9cf27c318446a50c663b57e92941ae035c
Ivan <adamovskij00@mail.ru>
2022-02-23 14:17:55 +0300

    add AnalogClockView file

---
+++ traning1/src/components/Clock/AnalogClockView.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {ClockViewPropsType} from "./Clock";
+
+export const AnalogClockView: React.FC<ClockViewPropsType> = ({date}) => {
+  return (
+    <span>
+      Analog clock
+    </span>
+  )
+}
